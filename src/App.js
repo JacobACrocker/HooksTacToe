2af9124215e98386ec3f65bdcd6d0f220a2d0cf9
Jacob Crocker <jacobcrocker93@gmail.com>
2019-04-17 09:17:58 -0500

    Fully converted to hooks. Random starting player.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Board from './Components/Board'
 import Counter from './Components/Count'
 import './App.css';
 
-class App extends Component {
+function App() {
 
-  state = {
-    gameBoard: Array(9).fill(''),
-    startingPlayer: null,
-    currentPlayer: 'X',
+  const randomizePlayer = () => {
+    if (number % 2 === 0) {
+      let player = 'X'
+      //message(`Player ${startingPlayer} goes first`)
+      console.log(`Player ${player} goes first`)
+      return(player)
+    } else {
+      let player = 'X'
+      //message(`Player ${startingPlayer} goes first`)
+      console.log(`Player ${player} goes first`)
+      return(player)
   }
+}
 
-  render() {
+    const [gameBoard, setGameBoard] = useState(Array(9).fill(''))
+    const [currentPlayer, setCurrentPlayer] = useState('')
+    const [number, setNumber] = useState(Math.floor(Math.random() * 10) + 1)
+    const [startingPlayer, setStartingPlayer] = useState(randomizePlayer())
+    //const [startingPlayer, setStartingPlayer] = useState(() => {const initialState = randomizePlayer()})
+    //const [gameWinner, setGameWinner] = useState(false)
+    
+    //const randomizePlayer = () => {
+      
+      
+    
 
-    console.log(`Using React version ${React.version}`);
+       
+      
 
     return (
       <div className="App">
@@ -25,12 +44,16 @@ class App extends Component {
             </p>
           </div>
         </header>
-        <Board data={this.state}/>
+        <div className='messageBoard'>
+          <div className='spacer'></div>
+          <div className='message'></div>
+          <div className='spacer'></div>
+        </div>
+        <Board currentPlayer={currentPlayer} startingPlayer={startingPlayer} gameBoard={gameBoard}/>
         <Counter player='X' initialCount={0}/>
         <Counter player='O' initialCount={0}/>
       </div>
     );
   }
-}
 
 export default App;
--- src/Components/Board.js
+++ src/Components/Board.js
@@ -3,21 +3,21 @@ import MessageBoard from './MessageBoard'
 
 function Board(props) {
 
-    const moves = props.data.gameBoard
+    const moves = props.gameBoard
 
-    const [player, setPlayer] = useState('i')
+    const [player, setPlayer] = useState(props.initialPlayer)
 
     //broken function
     const userDidMove = (index) => {
         function update() {
             setPlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X')
-            console.log(player)
+            console.log(`useDidMove->update() read player as ${player}`)
         }
         update()
         document.getElementById(`cell${index + 1}`).innerHTML = {player}
-        console.log(props.data.startingPlayer)
-        console.log(props.data.currentPlayer)
-        console.log(player)
+        console.log(`userDidMove read startingPlayer as ${props.startingPlayer}`)
+        console.log(`userDidMove read currentPlayer as ${props.currentPlayer}`)
+        console.log(`userDidMove read player as ${player}`)
     }
 
     const cell = moves.map((item, index) =>
