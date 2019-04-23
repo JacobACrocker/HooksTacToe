8124c2488fcb99cb3547a2e2e6690418197df075
Jacob Crocker <jacobcrocker93@gmail.com>
2019-04-23 14:42:55 -0500

    Added win conditions. Removed unneeded array.

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -5,25 +5,24 @@ const Board = () => {
 
     const [number, setNumber] = useState(Math.floor(Math.random() * 10) + 1)
 
-  function randomizePlayer() {
-    if (number % 2 === 0) {
-      let player = 'X'
-      //message(`Player ${startingPlayer} goes first`)
-      return(player)
-    } else {
-      let player = 'O'
-      //message(`Player ${startingPlayer} goes first`)
-      return(player)
-    } 
-  }   
+    function randomizePlayer() {
+        if (number % 2 === 0) {
+        let player = 'X'
+        //message(`Player ${startingPlayer} goes first`)
+        return(player)
+        } else {
+        let player = 'O'
+        //message(`Player ${startingPlayer} goes first`)
+        return(player)
+        } 
+    }   
 
-    const [initialGameBoard, setInitialGameBoard] = useState(Array(9).fill(''))
-    const [gameBoard, setGameBoard] = useState([])
+    const [gameBoard, setGameBoard] = useState(Array(9).fill(''))
     const [startingPlayer, setStartingPlayer] = useState(randomizePlayer())
     const [currentPlayer, setCurrentPlayer] = useState(startingPlayer)
 
     const blankCells = 
-        initialGameBoard.map((item, index) =>
+        gameBoard.map((item, index) =>
         <div className='cell'
             id={`cell${(index + 1)}`}
             key={index}
@@ -31,14 +30,34 @@ const Board = () => {
             {item}
         </div>
     )
-
-    //broken function 
+ 
     const userDidMove = (index) => {
-        const addMove = initialGameBoard.splice(index, 1, currentPlayer)
-        setGameBoard(addMove)
+        gameBoard.splice(index, 1, currentPlayer)
         updateBoard(currentPlayer)
-        switchPlayer(currentPlayer)       
+        switchPlayer(currentPlayer)     
     }
+
+    const winCheck = (player) => {
+        if ((gameBoard[0] === player && gameBoard[1] === player && gameBoard[2] === player) ||
+            (gameBoard[3] === player && gameBoard[4] === player && gameBoard[5] === player) ||
+            (gameBoard[6] === player && gameBoard[7] === player && gameBoard[8] === player) ||
+            (gameBoard[0] === player && gameBoard[3] === player && gameBoard[6] === player) ||
+            (gameBoard[1] === player && gameBoard[4] === player && gameBoard[7] === player) ||
+            (gameBoard[2] === player && gameBoard[5] === player && gameBoard[8] === player) ||
+            (gameBoard[0] === player && gameBoard[4] === player && gameBoard[8] === player) ||
+            (gameBoard[2] === player && gameBoard[4] === player && gameBoard[6] === player)
+            ){
+            console.log(player)
+            console.log(`winner`)
+            return(true)
+        } else {
+            console.log(player)
+            console.log(`no winner`)
+            return(false)
+        }
+    }
+
+    
 
        
     const updateBoard = () => {
@@ -52,6 +71,9 @@ const Board = () => {
                 {item}
             </div>)
 
+            console.log(gameBoard)
+            winCheck(currentPlayer)
+
             return(
                 <div>
                     {cells}
@@ -59,12 +81,15 @@ const Board = () => {
             )
     }
 
+    const arrayTestButton = () => {
+        console.log(gameBoard[0], gameBoard[1], gameBoard[2])
+    }
+
     const switchPlayer = () => {setCurrentPlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X')}
 
     const resetGame = () => {
         setNumber(Math.floor(Math.random() * 10) + 1)
-        setInitialGameBoard(Array(9).fill(''))
-        setGameBoard([])
+        setGameBoard(Array(9).fill(''))
         setStartingPlayer(randomizePlayer())
         setCurrentPlayer(startingPlayer)
     }
@@ -79,6 +104,7 @@ const Board = () => {
                 <div className='spacer'></div>
                 <div>
                     <button onClick={() => resetGame()}>Reset Game</button>
+                    <button onClick={() => arrayTestButton()}>Array Test</button>
                 </div>
             </div>
     ) 
