9283fc8fd02ff2bdcff48bc79f48506f0b9f23d5
Jacob Crocker <jacobcrocker93@gmail.com>
2019-04-26 16:40:13 -0500

    stuck before final render upon win condition

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -17,7 +17,7 @@ const Board = () => {
         } 
     }   
 
-    const [gameBoard, setGameBoard] = useState(Array(9).fill(''))
+    const [gameBoard, setGameBoard] = useState(Array(9).fill(null))
     const [startingPlayer, setStartingPlayer] = useState(randomizePlayer())
     const [currentPlayer, setCurrentPlayer] = useState(startingPlayer)
 
@@ -29,15 +29,43 @@ const Board = () => {
             onClick={() => userDidMove(index)}>
             {item}
         </div>
-    )
- 
+    )   
+ /*
     const userDidMove = (index) => {
-        gameBoard.splice(index, 1, currentPlayer)
-        updateBoard(currentPlayer)
-        switchPlayer(currentPlayer)     
+        console.log(`userDidMove ran`)
+        if (gameBoard[index] === null){
+            gameBoard.splice(index, 1, currentPlayer)
+            updateBoard()
+            winCheck(currentPlayer, index)
+            switchPlayer()
+        } else {
+            console.log(`cell full`)
+        }
+            console.log(`--------- end process -----------`)
+    }*/
+
+    const userDidMove = (index) => {
+        console.log(`userDidMove ran`)
+        if (gameBoard[index] === null){
+            gameBoard.splice(index, 1, currentPlayer)
+           if (winCheck(currentPlayer, index)){
+               console.log(`${currentPlayer} is the winner`)
+           } else {
+            switchPlayer()
+           }
+        } else {
+            console.log(`cell full`)
+        }
+            console.log(`--------- end process -----------`)
+    }
+    
+    const switchPlayer = () => {
+        setCurrentPlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X')
+        console.log(`switchPlayer ran`)
     }
 
-    const winCheck = (player) => {
+    const winCheck = (player, index) => {
+        console.log(`winCheck ran`)
         if ((gameBoard[0] === player && gameBoard[1] === player && gameBoard[2] === player) ||
             (gameBoard[3] === player && gameBoard[4] === player && gameBoard[5] === player) ||
             (gameBoard[6] === player && gameBoard[7] === player && gameBoard[8] === player) ||
@@ -47,53 +75,25 @@ const Board = () => {
             (gameBoard[0] === player && gameBoard[4] === player && gameBoard[8] === player) ||
             (gameBoard[2] === player && gameBoard[4] === player && gameBoard[6] === player)
             ){
+            console.log(gameBoard)
             console.log(player)
             console.log(`winner`)
             return(true)
         } else {
+            console.log(gameBoard)
             console.log(player)
             console.log(`no winner`)
             return(false)
         }
     }
 
-    
-
-       
-    const updateBoard = () => {
-        
-        const cells =
-            gameBoard.map((item, index) =>
-            <div className='cell'
-                id={`cell${(index + 1)}`}
-                key={index}
-                onClick={() => userDidMove(index)}>
-                {item}
-            </div>)
-
-            console.log(gameBoard)
-            winCheck(currentPlayer)
-
-            return(
-                <div>
-                    {cells}
-                </div>
-            )
-    }
-
-    const arrayTestButton = () => {
-        console.log(gameBoard[0], gameBoard[1], gameBoard[2])
-    }
-
-    const switchPlayer = () => {setCurrentPlayer(currentPlayer => currentPlayer === 'X' ? 'O' : 'X')}
-
     const resetGame = () => {
         setNumber(Math.floor(Math.random() * 10) + 1)
-        setGameBoard(Array(9).fill(''))
+        setGameBoard(Array(9).fill(null))
         setStartingPlayer(randomizePlayer())
         setCurrentPlayer(startingPlayer)
+        console.clear()
     }
-
 
     return (
         <div className='game' >
@@ -104,7 +104,6 @@ const Board = () => {
                 <div className='spacer'></div>
                 <div>
                     <button onClick={() => resetGame()}>Reset Game</button>
-                    <button onClick={() => arrayTestButton()}>Array Test</button>
                 </div>
             </div>
     ) 
